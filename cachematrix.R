c834423b44a42079a59a7d858d2b7322e985d3ea
jz042 <juliezhou@Julies-MacBook-Air.local>
2014-12-18 16:38:48 -0800

    First attempt at committing changes
    Incomplete, need to resolve issues

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,40 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The cachematrix function is a two step process:
+## 1. Creates a specific matrix and calculates its inverse
+## 2. Computes whether the inverse matrix has been cached and recalls it
 
-## Write a short comment describing this function
+## THIS FUNCTION CREATES A SPECIFIC MATRIX FOR WHICH WE WILL CALCULATE THE INVERSE MATRIX
 
-makeCacheMatrix <- function(x = matrix()) {
+makeCacheMatrix <- function(d,r,c) {
+  
+  x <- matrix(d,r,c)                        #The matrix is created here
+  
+  m <- NULL                                 #The solution that is set to NULL every time a new matrix is created
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() { x }                   #Returns the matrix that has been created
+  setsolve <- function(solve) m <<- solve   #Returns the inverse matrix the first time the cacheSolve function is called
+  getsolve <- function() m                  #Returns the inverse matrix the subsequent times the cacheSolve function is called
+  list(set = set, get = get,
+       setsolve = setsolve,
+       getsolve = getsolve)
 
 }
 
 
-## Write a short comment describing this function
+## THIS FUNCTION RECALLS THE CACHED INVERSE MATRIX SOLUTION (IF AVAILABLE)
+## IF NO SOLUTION HAS BEEN CACHED, IT WILL GENERATE A NEW SOLUTION
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x,...) {             ## Return a matrix that is the inverse of 'x'
+
+  m <- getsolve()                           #Returns the inverse matrix solution for the matrix that's been created
+  if(!is.null(m)) {                         #If the solution has been called once already
+    message("getting cached data")          #Return this message
+    return(m)                               #Then return the cached solution
+  }
+  data <- get()                             #Recalls the matrix created in the last step
+  m <- solve(data)                          #Generate a new solution
+  setsolve(m)                               #Store the new solution
+  m                                         #Return the new solution
 }
